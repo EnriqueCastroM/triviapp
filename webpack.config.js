e0ac0f4ace99e25478ead8961c40d8552701e257
EnriqueCastroM <kcmtha@gmail.com>
2023-04-14 16:08:19 -0600

    update:modeproduction

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   module: {
     rules: [
